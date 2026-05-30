b35cf431eb824109fb5cde6988257b6b6df48b35
Leandro Moraes <lbc_moraes@outlook.com>
2026-05-30 15:18:29 -0300

    feat(wallet): wire use cases and repository in AppModule

--- services/wallets/src/app.module.ts
+++ services/wallets/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { WalletsController } from "./presentation/controllers/wallets.controller";
+import { CreateWalletUseCase } from "./application/create-wallet.use-case";
+import { GetMyWalletUseCase } from "./application/get-my-wallet.use-case";
+import { WALLET_REPOSITORY } from "./application/wallet.repository";
 import { WalletOrmEntity, WalletReservationOrmEntity } from "./infrastructure/persistence/wallet-entities";
+import { CreateWalletTables1748476800000 } from "./infrastructure/persistence/migrations/1748476800000-CreateWalletTables";
+import { TypeOrmWalletRepository } from "./infrastructure/persistence/typeorm-wallet.repository";
+import { WalletsController } from "./presentation/controllers/wallets.controller";
 
 @Module({
   imports: [
@@ -9,9 +14,17 @@ import { WalletOrmEntity, WalletReservationOrmEntity } from "./infrastructure/pe
       type: "postgres",
       url: process.env.DATABASE_URL,
       synchronize: false,
+      migrations: [CreateWalletTables1748476800000],
+      migrationsRun: true,
       entities: [WalletOrmEntity, WalletReservationOrmEntity],
     }),
+    TypeOrmModule.forFeature([WalletOrmEntity, WalletReservationOrmEntity]),
   ],
   controllers: [WalletsController],
+  providers: [
+    { provide: WALLET_REPOSITORY, useClass: TypeOrmWalletRepository },
+    CreateWalletUseCase,
+    GetMyWalletUseCase,
+  ],
 })
 export class AppModule {}
