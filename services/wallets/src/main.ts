551c284189a5f091a66b879337f37778f002b2cf
Leandro Moraes <lbc_moraes@outlook.com>
2026-05-30 13:57:28 -0300

    chore(wallet): add migration script and bootstrap config

--- services/wallets/src/main.ts
+++ services/wallets/src/main.ts
@@ -1,8 +1,13 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { AppDataSource } from "./infrastructure/persistence/data-source";
 
 async function bootstrap(): Promise<void> {
+  const ds = await AppDataSource.initialize();
+  await ds.runMigrations();
+  await ds.destroy();
+
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
   await app.listen(port, "0.0.0.0");
